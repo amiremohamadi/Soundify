33a3face984cddf32c2cc4885fc10208aeb7ab54
amiremohamadi <amiremohamadi@yahoo.com>
2020-11-15 06:33:02 +0330

    play sound in another process :rocket:

--- soundify/soundify.py
+++ soundify/soundify.py
@@ -8,9 +8,22 @@ changelogs:
     - nothing
 '''
 
-from config import read_config
-from subprocess import Popen
+from .config import read_config
+from subprocess import Popen, DEVNULL
 from playsound import playsound
+
+
+def play(code):
+    '''
+    play a sound by exitcode
+    '''
+    # a map from error_code to sound_dir
+    err_sound = read_config()
+    print(err_sound)
+    
+    sound = err_sound.get(code, None)
+    if sound is not None:
+        playsound(sound)
 
 
 def soundify(command):
@@ -18,21 +31,14 @@ def soundify(command):
     soundify a command.
     run command as another process, check exit code and play related sound
     '''
-
-    # a map from error_code to sound_dir
-    err_sound = read_config()
-
     # run the command in new process
-    proc = Popen(command)
+    proc = Popen(command.split())
     proc.wait()
 
     # capture return code, play sound
     ret = proc.returncode
 
-    sound = err_sound.get(ret, None)
-    if sound:
-        # TODO: Popen playsound
-        pass
+    Popen(['soundify', '--test', str(ret)], stderr=DEVNULL, stdout=DEVNULL)
 
     # keep return code
     return ret
