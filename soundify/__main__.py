cd1a266d3c0764b2290d15dfaccd4ce95b646ccb
amiremohamadi <amiremohamadi@yahoo.com>
2020-11-15 08:37:58 +0330

    show condif directory flag :tada:

--- soundify/__main__.py
+++ soundify/__main__.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from .soundify import soundify, play
+from .config import CONFIG_FILE
 
 __author__ = '@amiremohamadi'
 __version__ = '0.1'
@@ -22,9 +23,15 @@ def main():
     parser = ArgumentParser(description=__description__)
     parser.add_argument('-v', '--version', action='version', version=__version__)
     group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-c', '--config-file', action='store_true')
     group.add_argument('-t', '--test', nargs='?', type=int, help='test exitcode sound')
     group.add_argument('command', nargs='?', type=str, help='command to run')
     args = parser.parse_args()
+
+    # show config directory
+    if args.config_file:
+        print(CONFIG_FILE)
+        return 0
 
     # with --test/-t flag you can test an exitcode sound
     if args.test is not None:
